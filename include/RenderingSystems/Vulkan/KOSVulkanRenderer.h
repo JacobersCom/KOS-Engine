e9e85c139c1742ea2b71d177a43aa1fa249e317e
JacoberCom <jacobymartin1@icloud.com>
2026-04-08 22:32:06 -0400

    Begined working on the renderer to get the window up and running. Going to work on the renderManager to handle does APIs with #defines

--- include/RenderingSystems/Vulkan/KOSVulkanRenderer.h
+++ include/RenderingSystems/Vulkan/KOSVulkanRenderer.h
@@ -4,13 +4,23 @@ class VulkanBuffers; // May remove
 
 /*
 VulkanRenderer will is independed and can start up first
-VulkanRenderer handles all the graphics operations such as
--Pipeline Creation
--RenderPasses
--Draw calls
+VulkanRenderer handles all the graphics operations and 
+instance creation
 */
 class VulkanRenderer {
 
 public:
+
+	VkInstance KInstance;
+	bool KActive;
+
+
+	void Init();
+
+private:
+
+	VulkanRenderer() {};
+	~VulkanRenderer() {};
+
 
 };
--- src/RenderingSystems/Vulkan/KOSVulkanRenderer.cpp
+++ src/RenderingSystems/Vulkan/KOSVulkanRenderer.cpp
@@ -1,9 +1,15 @@
-#include <vulkan/vulkan.h>
+#include "KOSVulkanPrerequisites.h"
+#include "KOSVulkanRenderer.h"
 
-
-class KOSVulkanRenderer
+void VulkanRenderer::Init()
 {
-	KOSVulkanRenderer();
+	VkApplicationInfo AppInfo{};
 
+	AppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	AppInfo.pNext = VK_NULL_HANDLE;
+	AppInfo.pApplicationName = "KOS Engine";
+	AppInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	AppInfo.apiVersion = VK_API_VERSION_1_4;
 
-};
+	VkInstance InstanceInfo{};
+}
