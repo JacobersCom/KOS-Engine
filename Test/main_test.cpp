4a932bb6b43de039603c3c1495e6878e3046d1b1
JacoberCom <jacobymartin1@icloud.com>
2026-04-15 08:52:49 -0400

    Added copyrights to main_test file

--- Test/main_test.cpp
+++ Test/main_test.cpp
@@ -1,7 +1,19 @@
-/*
-1. Configure with tests ON (default)
-cmake -B build -S . -DBUILD_TESTS=ON
+/**
+ * @ Author Hunter Wilkinson
+ * @ Create Time: 2026-04-15 08:23:00
+ * 
+ * Main_test.cpp is for all testing of all my systems
+ * 
+ * 1. Configure with tests ON (default)
+ * cmake -B build -S . -DBUILD_TESTS=ON
+ * 2. Now turn tests OFF
+ * cmake -B build -S . -DBUILD_TESTS=OFF
+ */
 
-2. Now turn tests OFF
-cmake -B build -S . -DBUILD_TESTS=OFF
-*/
+
+#include "Engine-Core/include/ecs/KECS.h"
+
+int main()
+{
+    
+}
