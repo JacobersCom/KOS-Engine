f84bf3b1907724f517dbb7703b08ccbd43f5c45f
JacoberCom <jacobymartin1@icloud.com>
2026-05-11 07:51:11 -0400

    [KOSAudio] Started getting the headers and compoentObject models and identifiers needed for the Audio system.

--- Engine-Core/src/KOSAudio.cpp
+++ Engine-Core/src/KOSAudio.cpp
@@ -6,7 +6,28 @@
 #include <Audioclient.h>
 #include <audiopolicy.h>
 
+//ComponentObjectModel Files
+#include <objbase.h>
+
+//interface Identifiers
+constexpr IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
+
+//CLSID is used to identify GUIDs 
+constexpr CLSID CLSID_MMDEviceEnumerator = 
+
+
+
 KOSAudio::KOSAudio()
 {
 
+}
+
+KOSAudio::~KOSAudio()
+{
+
+}
+
+void KOSAudio::CreateAudioStream()
+{
+	
 }
