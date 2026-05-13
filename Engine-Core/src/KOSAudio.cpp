7260d2398b71841565ac0754be1daa1ad337d0a2
JacoberCom <jacobymartin1@icloud.com>
2026-05-13 07:48:00 -0400

    [AUDIO] Created all interface Identifiers.

--- Engine-Core/src/KOSAudio.cpp
+++ Engine-Core/src/KOSAudio.cpp
@@ -5,17 +5,18 @@
 //WASAPI interfaces
 #include <Audioclient.h>
 #include <audiopolicy.h>
+#include <mmdeviceapi.h>
 
 //ComponentObjectModel Files
 #include <objbase.h>
 
+//CLSID is used to identify GUIDs 
+constexpr CLSID CLSID_MMDEviceEnumerator = __uuidof(MMDeviceEnumerator);
+
 //interface Identifiers
 constexpr IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
-
-//CLSID is used to identify GUIDs 
-constexpr CLSID CLSID_MMDEviceEnumerator = 
-
-
+constexpr IID IID_IAudioClient = __uuidof(IAudioClient);
+constexpr IID IID_IMMDeviceEnumrator = __uuidof(IMMDeviceEnumerator);
 
 KOSAudio::KOSAudio()
 {
