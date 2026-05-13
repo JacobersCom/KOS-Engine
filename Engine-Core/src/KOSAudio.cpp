8212c0521bc86969b245cf00498d2d208cdf68c8
JacoberCom <jacobymartin1@icloud.com>
2026-05-13 08:06:16 -0400

    [AUDIO] Commented out data types for the Audio WASAPI.

--- Engine-Core/src/KOSAudio.cpp
+++ Engine-Core/src/KOSAudio.cpp
@@ -11,11 +11,12 @@
 #include <objbase.h>
 
 //CLSID is used to identify GUIDs 
-constexpr CLSID CLSID_MMDEviceEnumerator = __uuidof(MMDeviceEnumerator);
+constexpr CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
 
 //interface Identifiers
 constexpr IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
 constexpr IID IID_IAudioClient = __uuidof(IAudioClient);
+//Interface to multimedia devices
 constexpr IID IID_IMMDeviceEnumrator = __uuidof(IMMDeviceEnumerator);
 
 KOSAudio::KOSAudio()
@@ -31,13 +32,26 @@ KOSAudio::~KOSAudio()
 void KOSAudio::CreateAudioStream()
 {
 	HRESULT hr;
+	//Uint64_t type used for clock time
 	REFERENCE_TIME RequestedDuration = 10000000;
 	REFERENCE_TIME ActualDuration;
+
 	IMMDeviceEnumerator* pEnumerator = nullptr;
+
+	//Used to interface with the audio end points
 	IMMDevice* pDevice = nullptr;
+
+	//Used to inerface to create and initialize an audio stream
+	//Between the audio app and engine
 	IAudioClient* pAudioClient = nullptr;
+
+	//Used to enable client write output data to a rendering
+	//end point
 	IAudioRenderClient* pAudioRenderer = nullptr;
+
+	//Defines the format of waveform-audio data
 	WAVEFORMATEX* pwfx = nullptr;
+
 	UINT32 bufferFrameCount;
 	UINT32 numFramesAvailable;
 	UINT32 numFramesPadding;
