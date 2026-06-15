dcb397ac8eccce3fb8259fb3c976ce76301525cc
Hunter Wilkinson <jacobymartin1@icloud.com>
2026-06-14 21:01:53 -0400

    Swapchain class created

--- src/KSwapchain.cpp
+++ src/KSwapchain.cpp
@@ -1,1 +1,102 @@
-#include "KSawpchain.h"
+#include "KSwapchain.h"
+#include "KWindow.h"
+
+Kos::KSwapchain::KSwapchain()
+{
+}
+
+Kos::KSwapchain::~KSwapchain()
+{
+
+}
+
+
+bool Kos::KSwapchain::GetSwapchainDetails(VkPhysicalDevice device, VkSurfaceKHR surface)
+{
+
+	//Get the capabilities of the users surfaces
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.SurfaceCapabilities);
+
+	//Query color formats supported by surface
+	uint32_t formatCount;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+
+	if (formatCount == 0)
+	{
+		return false;
+	}
+	details.ImageFormats.resize(formatCount);
+	vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.ImageFormats.data());
+
+	uint32_t presentCount;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentCount, nullptr);
+
+	if (presentCount == 0)
+	{
+		return false;
+	}
+	details.PresentMode.resize(presentCount);
+	vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentCount, details.PresentMode.data());
+
+	return true;
+
+}
+
+VkSurfaceFormatKHR Kos::KSwapchain::ChooseSwapchainFormat(const std::vector<VkSurfaceFormatKHR> formats)
+{
+	for (const auto availableFormat : formats)
+	{
+		if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB
+			&& availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) // applys support for images in SRGB color space
+		{
+			return availableFormat;
+		}
+		return formats[0];
+	}
+}
+
+
+VkPresentModeKHR Kos::KSwapchain::ChooseSwapchainPresentMode(const std::vector<VkPresentModeKHR> present)
+{
+	for (const auto& availablePresentMode : details.PresentMode)
+	{
+		//Single entry queue for presentation request
+		if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
+		{
+			return availablePresentMode;
+		}
+	}
+	return VK_PRESENT_MODE_FIFO_KHR; //New request are appended to the end of the queue and the beginning one is removed
+}
+
+
+VkExtent2D Kos::KSwapchain::ChooseSwapchainExtent(const VkSurfaceCapabilitiesKHR capabilities)
+{
+	//Allocate memory to window pointer
+	window = std::make_unique<KWindow>();
+
+	if (capabilities.currentExtent.width != UINT32_MAX)
+	{
+		return capabilities.currentExtent;
+	}
+	else
+	{
+		//Gets the frame buffer size and stores in within width and height
+		int width, height;
+		window->GetFrameBufferSize(window->GetWindowHandle(), width, height);
+
+		VkExtent2D actualExtent =
+		{
+			static_cast<uint32_t>(width),
+			static_cast<uint32_t>(height)
+
+		};
+
+		actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+		return actualExtent;
+
+	}
+}
+
--- src/KSwapchain.h
+++ src/KSwapchain.h
@@ -1,23 +1,57 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
 namespace Kos
 {
-	class KSawpchain
+	class KSwapchain
 	{
-		KSawpchain();
-		~KSawpchain();
+		KSwapchain();
+		~KSwapchain();
 
-		bool GetSawpchainDetails(VkPhysicalDevice device);
+		/*
+		Gets all the physical devices information need for the swapchain.
+		Such as surface capabilities, format and present modes
+		@param
+		- VkPhysical - handle to the users physical device
+		@param
+		- VkSurfaceKHR - handle to the window surface
+		*/
+		bool GetSwapchainDetails(VkPhysicalDevice device, VkSurfaceKHR surface);
+		
+		/*
+		Chooses the supported image format for the swapchain
+		@param
+		- Vector of VkSurfaceFormatKHR
+		*/
+		VkSurfaceFormatKHR ChooseSwapchainFormat(const std::vector<VkSurfaceFormatKHR> formats);
+		
+		/*
+		Chooses the supported present modes for the swapchain
+		@param
+		- Vector of VkPresentModeKHR
+		*/
+		VkPresentModeKHR ChooseSwapchainPresentMode(const std::vector<VkPresentModeKHR> present);
+		
+		/*
+		Choose the Swapchain extent which is the size of the images vulkan will present
+		*/
+		VkExtent2D ChooseSwapchainExtent(const VkSurfaceCapabilitiesKHR capabilities);
 
 	private:
 
-
-
-		struct SawpchainDetails
+		struct SwapchainDetails
 		{
+			VkSurfaceCapabilitiesKHR SurfaceCapabilities;
+			std::vector<VkSurfaceFormatKHR> ImageFormats;
+			std::vector<VkPresentModeKHR> PresentMode;
+		};
 
-		}
+		SwapchainDetails details;
+
+		std::unique_ptr<class KWindow> window;
 	};
 }
