57e1d9a7ca68be0621a740c0319c0b5505417c1e
Jacoby <jacobymartin1@proton.me>
2026-09-24 11:54:01 -0400

    Create a batch file to help speed up the build process

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+set "build_folder=build"
+
+if exist "%build_folder%" (
+	echo found build folder. compiling project...
+	cmake --build build
+) else (
+
+	 set /p version="Enter Visual Studio verison (22 or 26): "
+	if "%version%"=="22" (
+		cmake -G "Visual Studio 17 2022" -B build
+		cmake --build build
+	) else if "%version%"=="26" (
+		cmake -G "Visual Studio 18 2026" -A x6x -B build
+	 	cmake --build build
+	) else (
+		echo Invalid version. Please enter 22 or 26.
+	)
+)
+
+
